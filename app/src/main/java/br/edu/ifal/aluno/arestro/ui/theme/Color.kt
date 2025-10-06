e86b81961414b84b74414ca1abbf265ba2a4248f
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-10-05 23:30:38 -0300

    feat: Add initial color definitions

--- app/src/main/java/br/edu/ifal/aluno/arestro/ui/theme/Color.kt
+++ app/src/main/java/br/edu/ifal/aluno/arestro/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Green30 = Color(0xFF00CC55)
+val Green50 = Color(0xFF009944)
+val Green90 = Color(0xFF004422)
+
+val Gray10 = Color(0xFFCDD1E0)
+val Gray25 = Color(0xFF999999)
+val Gray50 = Color(0xFF666666)
+val Gray75 = Color(0xFF383838)
+
+val Red25 = Color(0xFFE86969)
+
+val Blue50 = Color(0xFF2F89FC)
