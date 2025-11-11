dbaf0bdf4d5d8e791410371ea97d0aab7b853d9f
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-11-11 17:53:16 -0300

    feat: Add navigation for Home screen and integrate layout component

--- app/src/main/java/br/edu/ifal/aluno/arestro/navigation/ArestroNavHost.kt
+++ app/src/main/java/br/edu/ifal/aluno/arestro/navigation/ArestroNavHost.kt
@@ -4,17 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import br.edu.ifal.aluno.arestro.components.base.Layout
+import br.edu.ifal.aluno.arestro.screens.HomeScreen
 import br.edu.ifal.aluno.arestro.screens.OnBoardingScreen
 import kotlinx.serialization.Serializable
 
 @Serializable
 object OnBoardingRoute
 
+@Serializable
+object HomeRoute
+
 @Composable
 fun ArestroNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = OnBoardingRoute) {
         composable<OnBoardingRoute> {
-            OnBoardingScreen()
+            Layout(showAppBar = false) {
+                OnBoardingScreen()
+            }
+        }
+        composable<HomeRoute> {
+            Layout(showAppBar = true) {
+                HomeScreen()
+            }
         }
     }
 }
