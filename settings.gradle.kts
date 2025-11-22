8945bc0799656d7833fddded8bdaa0d551b9e2db
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-11-21 23:34:37 -0300

    chore: Downgrade AGP version from 8.13.1 to 8.13.0 and clean up settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,19 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
 rootProject.name = "ARestro"
 include(":app")
-
-annotation class mavenCentral
